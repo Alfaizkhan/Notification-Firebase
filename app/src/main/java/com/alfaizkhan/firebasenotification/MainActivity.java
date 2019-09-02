cebf47672c6909b90ddf62b1684cff10d0d6833a
Alfaizkhan <alfaizkhan2810@gmail.com>
2019-09-02 15:06:00 +0530

    Code Added

---
+++ app/src/main/java/com/alfaizkhan/firebasenotification/MainActivity.java
@@ -0,0 +1,22 @@
+package com.alfaizkhan.firebasenotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.gms.common.internal.Constants;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        }
+
+    }
